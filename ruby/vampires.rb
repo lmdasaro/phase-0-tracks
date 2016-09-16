5769d492681d4d421a06845210a07c059bdc8308
Lisa D'Asaro <lmdasaro@gmail.com>
2016-09-16 16:48:06 -0700

    Modify vampires file to include questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?(y/n)"
+order_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?(y/n)"
+insurance = gets.chomp
