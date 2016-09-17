27cba9fecf874f3264123da29e4b6fcf493bb39b
Lisa D'Asaro <lmdasaro@gmail.com>
2016-09-17 12:52:54 -0700

    Modify vampires file to complete release 2.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,7 +2,7 @@ real_name = true
 real_age = true
 garlic_bread = true
 sign_up = true
-
+result = "Result inconclusive."
 
 puts "What is your name?"
 name = gets.chomp
@@ -29,3 +29,20 @@ insurance = gets.chomp
 if (insurance == "n")
         sign_up = false
 end
+
+
+if real_age && ( garlic_bread || sign_up )
+	result = "Probably not a vampire."
+end
+if !real_age && ( !garlic_bread || !sign_up )
+	result = "Probably a vampire."
+end
+if !real_age && !garlic_bread && !sign_up
+	result = "Almost certainly a vampire."
+end
+if !real_name
+	result = "Definitely a vampire."
+end
+
+
+puts "#{result}"
