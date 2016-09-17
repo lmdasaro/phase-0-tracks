d56df4fbe49f9caa46a0f0dc81c208bb5b876aff
Lisa D'Asaro <lmdasaro@gmail.com>
2016-09-17 12:31:20 -0700

    Add some detection logic from release 2.2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,31 @@
+real_name = true
+real_age = true
+garlic_bread = true
+sign_up = true
+
+
 puts "What is your name?"
 name = gets.chomp
+name = name.downcase
+if (name == "drake cula") || (name == "tu fang")
+		real_name = false
+end
 
-puts "How old are you?"
+puts "How old are you? What year were you born?"
 age = gets.chomp
-
-puts "What year were you born?"
 year = gets.chomp
+if (2016 - year.to_i) != age.to_i
+	    real_age = false
+end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?(y/n)"
 order_bread = gets.chomp
+if (order_bread == "n")
+        garlic_bread = false
+end
 
 puts "Would you like to enroll in the company's health insurance?(y/n)"
 insurance = gets.chomp
+if (insurance == "n")
+        sign_up = false
+end
