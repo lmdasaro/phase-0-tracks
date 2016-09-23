6e86e168a180fcbf52e94be31d19052993435865
Lisa D'Asaro <lmdasaro@gmail.com>
2016-09-23 13:49:24 -0700

    Modify nested data structure to include printed access to deeply nested data.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -15,3 +15,45 @@ backyard = {
 	main: ['grass', 'paved stones', 'wooden planks'],
 	gate: ['compost bin', 'trash bin', 'recycling bin', 'crv bin']
 }
+
+puts
+puts
+
+backyard[:vegetable_garden][:bed][3] = 'zuccini'
+puts backyard
+
+puts
+puts
+
+backyard[:patio][:refrigerator][:bottom].each do |cold_content|
+	puts "#{cold_content.capitalize} is in the chilled compartment."
+end
+
+puts 
+puts
+
+backyard[:patio][:refrigerator][:top].each do |frozen_content|
+	puts "#{frozen_content.capitalize} is in the frozen compartment."
+end
+
+puts
+puts
+
+backyard[:corner_trees].push('apple')
+puts backyard
+
+puts 
+puts
+
+puts backyard[:patio][:furniture]
+
+puts 
+puts
+
+backyard[:gate][-1].sub!("crv", "CRV")
+puts backyard[:gate]
+
+puts
+puts
+
+puts "#{backyard[:vegetable_garden][:wall][-1].capitalize!} is great for burns and moisturizing skin."
