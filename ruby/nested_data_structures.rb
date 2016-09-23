9d254ade7488ee81aab0f00e46e3606120794ee6
Lisa D'Asaro <lmdasaro@gmail.com>
2016-09-23 12:42:37 -0700

    Create nested data structure of backyard for 5.4 assignment.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+backyard = {
+	vegetable_garden: {
+		bed: ['tomato', 'eggplant', 'pepper', 'squash', 'chives', 'lettuce', 'cucumber'],
+		wall: ['green onion', 'kale', 'aloe vera']
+	},
+	corner_trees: ['date', 'lemon', 'orange', 'apricot'],
+	patio: {
+		refrigerator: {
+			bottom: ['bottled water', 'smoothies', 'tofu', 'cheese', 'lunch meat'],
+			top: ['ice', 'frozen vegetables', 'lean pockets', 'frozen chicken breasts']
+		},
+		potted_plants: ['lily', 'tea roses', 'cactus'],
+		furniture: ['grill', 'shelves','table', 'chair_1', 'chair_2', 'chair_3', 'chair_4']
+	},
+	main: ['grass', 'paved stones', 'wooden planks'],
+	gate: ['compost bin', 'trash bin', 'recycling bin', 'crv bin']
+}
