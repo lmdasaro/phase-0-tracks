fb9c7da26b6d22915926a9289e14ce9f1cf019f4
Lisa D'Asaro <lmdasaro@gmail.com>
2016-09-28 00:39:51 -0700

    Create class of puppy for 5.1 and puppy and tiger classes for 5.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,105 @@
+
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+def  initialize
+puts "Initializing new puppy instance ..."
 
 end
 
+def fetch(toy)
+puts "I brought back the #{toy}!"
+toy
+end
+
+def speak(number) 
+count = 0
+until count >= number
+
+puts "Woof!"
+count+= 1
+end
+
+# number.times{puts "Woof!"}
+
+end
+
+def roll_over
+puts "*rolls over*"
+
+end
+
+def dog_years(age)
+age * 7
+end
+
+def high_five
+puts "*gives high five*"
+end
+
+end
+
+
+# ############ DRIVER  CODE ##########
+
+puppy = Puppy.new
+
+puppy.fetch("ball")
+puppy.speak(5)
+puppy.roll_over
+puts puppy.dog_years(10)
+puppy.high_five
+Yuujiro = Puppy.new
+
+#================================  NEW TIGER CLASS  ========================================#
+
+class Tiger
+
+def initialize
+puts "Initializing new tiger instance ..."
+end
+
+def roar(number)
+number.times{puts "*roar*"}
+end
+
+def high_jump
+puts "*jumps really high*"
+end
+end
+
+
+######## DRIVER CODE #########
+
+
+
+count = 1
+
+
+arr = []
+#alternate loop style:
+# loop do 
+# break if count == 50
+# end
+
+until count > 50
+
+puts "Instance Name?"
+name = gets.chomp
+name = Tiger.new
+
+arr << name
+# puts instance_name
+
+count += 1
+
+
+end
+
+# puts arr
+
+arr.each do |name|
+
+name.roar(2)
+name.high_jump
+
+end
