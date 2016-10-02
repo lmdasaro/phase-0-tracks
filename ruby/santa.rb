0b1fbc55552ec0546d4f6d8d32aa8f299924d401
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-01 22:11:38 -0700

    Modify genders, ethnicities, and add attribute-changing methods to Santa class.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,23 +1,44 @@
 class Santa
 
-def  initialize (gender, ethnicity)
-puts "Initializing Santa instance ..."
-@gender = gender
-@ethnicity = ethnicity
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-age = 0
+	def  initialize (gender, ethnicity)
+		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		age = 0
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+	end
+
+	def celebrate_birthday
+		age += 1
+	end
+
+	def get_mad_at(reindeer)
+
+	end
+
+	def gender
+		@gender = gender
+	end
+	
 end
 
-def speak
-puts "Ho, ho, ho! Haaaappy holidays!"
-end
+# Nick = Santa.new
+# Nick.speak
+# Nick.eat_milk_and_cookies("oatmeal raisin")
+santas = []
 
-def eat_milk_and_cookies(cookie_type)
-puts "That was a good #{cookie_type}!"
-end
+example_genders = ["agender", "female", "ladyboy", "bigender", "male", "lady_gaga", "female", "banana", "gender fluid", "N/A", "agender", "female"]
+example_ethnicities = ["black", "Lithuanian", "Thai", "Latino", "white", "Japanese-African", "prefer not to say", "fruit", "Mystical Creature (unicorn)", "N/A", "robot", "Kitten"]
 
-end
-
-Nick = Santa.new
-Nick.speak
-Nick.eat_milk_and_cookies("oatmeal raisin")
+example_genders.length.times do |i|
+  	santas << Santa.new(example_genders[i], example_ethnicities[i])
+  end
+p santas
