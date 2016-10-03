eabd924da3c5f32cfd029e5f5f3a6c2fca344388
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-03 08:16:15 -0700

    Update hangman file with guess letter method.

--- ruby/game/hangman.rb
+++ ruby/game/hangman.rb
@@ -1,14 +1,36 @@
 class Hangman
-	attr_reader :guess_count, :game_over :answer
+	attr_reader :guess_count, :game_over, :answer, :guess_display
 
 	def initialize(answer)
 		@answer = answer.downcase.split("")
-		@guess_max = @answer.length
+		@guess_max = (answer.length.to_i - 1)
 		@guess_count = 0
 		@guess_array = []
+		@correct_letter_index = []
+		@guess_display = ['_'] * answer.length
 		@game_over = false
 	end
-
+	def correct_answer
+		@guess_display == @answer || !@guess_display.include?('_')
+	end
+	def repeat_letter(letter)
+		@guess_array.include?(letter)
+	end
+	def guess_by_letter(letter)
+		index = 0	
+		@guess_array << letter
+		@answer.each do |input|
+			if input == letter
+				@correct_letter_index << index
+			end
+			index += 1
+		end
+	 	@correct_letter_index.each do |index|
+	 		@guess_display[index] = @answer[index]
+	 	end
+ 		@guess_max -= 1	
+ 		@guess_display.join(' ')
+	end
 	def check_word(letter)
 		@guess_count += 1
 		if @word == #{answer}
@@ -17,6 +39,7 @@ class Hangman
 			false
 		end
 	end
+	
 
 end
 
@@ -25,7 +48,7 @@ puts "Let's play some hangman!"
 game = Hangman.new
 puts "Player 1, please enter a word."
 answer = gets.chomp
-puts "Player 2, you have #{game.guess} guesses."
+puts "Player 2, you have #{game.guess_max} guesses."
 
 while !game.game_over
 	puts "Player 2, please try and guess the word by entering a letter."
