9a008879c293397ffa4a4e6adb6c2e2bd1a11107
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-03 04:25:00 -0700

    Modify methods for repeats.

--- ruby/game/hangman.rb
+++ ruby/game/hangman.rb
@@ -1,9 +1,11 @@
 class Hangman
-	attr_reader :guess_count, :game_over
+	attr_reader :guess_count, :game_over :answer
 
-	def initialize
-		@word
+	def initialize(answer)
+		@answer = answer.downcase.split("")
+		@guess_max = @answer.length
 		@guess_count = 0
+		@guess_array = []
 		@game_over = false
 	end
 
@@ -23,7 +25,7 @@ puts "Let's play some hangman!"
 game = Hangman.new
 puts "Player 1, please enter a word."
 answer = gets.chomp
-
+puts "Player 2, you have #{game.guess} guesses."
 
 while !game.game_over
 	puts "Player 2, please try and guess the word by entering a letter."
