0e407975b538342f89f61f3e43897d6617405ace
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-02 18:21:45 -0700

    Finish release 3 to complete file for 6.4 with mixin module working on two different classes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,31 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yell_happily(words)
+#   	words + "!!!" + " :D"
+#   end
+# end
+
+# puts Shout.yell_angrily("This is so stressful")
+# puts Shout.yell_happily("People are so nice sometimes")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yell_happily(words)
-  	words + "!!!" + " :D"
-  end
+	def yells(words)
+		words.upcase + "!!" 
+	end
 end
 
-puts Shout.yell_angrily("This is so stressful")
-puts Shout.yell_happily("People are so nice sometimes")
+class Mother
+	include Shout
+end
+
+class Daughter
+	include Shout
+end
+
+Hyang = Mother.new
+Lisa = Daughter.new
+
+p Hyang.yells("I don't like this")
+p Lisa.yells("You don't have to")
