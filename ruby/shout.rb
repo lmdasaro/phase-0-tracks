de1a8dba33c22500d02e07c50c3bf4c153cd5be5
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-02 17:37:26 -0700

    Add driver code to test module methods for release 1.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,4 +2,10 @@ module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+  def self.yell_happily(words)
+  	words + "!!!" + " :D"
+  end
 end
+
+puts Shout.yell_angrily("This is so stressful")
+puts Shout.yell_happily("People are so nice sometimes")
