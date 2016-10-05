2c9ae99a8d3ac30941465b54fc31f3a3399e27a9
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-05 10:42:30 -0700

    Create and modify explore.js file to create a basic reverse method.

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+//I need to take a string as a parameter and reverse it! ex: reverse("hello") should return "olleh".
+//for each letter, I need to print it to the console in reverse.
+
+var str = "hello";
+function reverse(str) {
+	for (var i =str.length; i < str.length; i++) 
+	console.log(i); 
+	return (str).reverse;
+}
