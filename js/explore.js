35085697acea4564b49fd23a6c1f15afb261818c
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-05 11:12:13 -0700

    Modify file to print results to the console.

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,18 @@
 //I need to take a string as a parameter and reverse it! ex: reverse("hello") should return "olleh".
 //for each letter, I need to print it to the console in reverse.
 
-var str = "hello";
+//'' + 'a' would result in the string 'a'
+
+
+// testing var str = "hello";
+
+
 function reverse(str) {
-	for (var i =str.length; i < str.length; i++) 
-	console.log(i); 
-	return (str).reverse;
+	var reverseString =  '';
+	for (var i =str.length-1; i >= 0; i--) 
+	reverseString += str[i];
+	return reverseString;
 }
+console.log(reverse("hello"));
+
+console.log(reverse("Lisa D'Asaro"));
