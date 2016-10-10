733d1a4b2f6508cd41b02f5c64d5d195798c7e65
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-10 07:01:49 -0700

    Add algos.js file and complete first function with test and pseudocode for release 0.

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+//A function needed that takes the longest word in an array and returns the word with the greatest length. I'll need to iterate through an array and determine which word has the greatest length value, then return that word.
+
+function longestWord(arr){
+	var longestWord = ""
+	for(var i = 0; i < arr.length; i++){
+		if(arr[i].length > longestWord.length){
+			longestWord = arr[i];
+		}
+	}
+
+	return longestWord;
+}
+//test code
+console.log(longestWord(["apple", "banana", "cat", "dog", "elephant"]))
+console.log(longestWord(["short", "long", "extraneous"]))
+//A function needed that takes two objects and checks to see if the objects share at least one key-value pair. 
