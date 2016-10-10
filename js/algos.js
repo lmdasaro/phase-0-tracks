a571a54a0819bf93e9c327a205424dce9aaf6601
Lisa D'Asaro <lmdasaro@gmail.com>
2016-10-10 07:22:24 -0700

    Modify algos.js file to complete second function with pseudocode and test code for release 1.

--- js/algos.js
+++ js/algos.js
@@ -14,7 +14,9 @@ function longestWord(arr){
 console.log(longestWord(["apple", "banana", "cat", "dog", "elephant"]))
 console.log(longestWord(["short", "long", "extraneous"]))
 
-//A function needed that takes two objects and checks to see if the objects share at least one key-value pair. 
+
+
+//A function needed that takes two objects and checks to see if the objects share at least one key-value pair. If a key from object 1 has the same key as object 2, return a boolean of true. Otherwise, return false.
 
 function pairShare(obj1, obj2){
 	for(var key in obj1) {
@@ -25,4 +27,5 @@ function pairShare(obj1, obj2){
 	return false;
 }
 //test code
-console.log(pairShare())
+console.log(pairShare({name: "Steven", age: 54},{name: "Tamir", age: 54}))
+console.log(pairShare({name: "Lisa", sex: "f"}, {name: "Tia", sex: "f"}, {name: "Steven", sex: "m"}))
