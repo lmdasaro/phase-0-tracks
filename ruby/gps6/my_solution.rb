3f0ab2513881bcb26f736ed5e1bb5cf9d978c9eb
Lisa D'Asaro <lmdasaro@gmail.com>
2016-11-16 17:59:27 -0800

    upload gps assignment.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,29 +1,29 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Alex Gonzalez].
+# We spent [2] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Require_relative links files together, getting data from another file.
+# Require_relative links a file in the same directory. Require links a file from a different location. Require can also pull in ruby files, gems, etc.
 require_relative 'state_data'
 
 class VirusPredictor
-
+# We are initializing a new instance of our VirusPredictor class with input of state_of_origin, population_density, and population.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+# Virus_effects show us the output of the information from a particular state. It shows us the predicted deaths and speed of spread.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
-
-  def predicted_deaths(population_density, population, state)
+# This method predicts the deaths based on population density and uses the floor method to round down to the nearest whole number. It then prints how many deaths will occur in that state due to the virus outbreak.
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -40,8 +40,8 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
-  def speed_of_spread(population_density, state) #in months
+# This method is predicting the speed of spread based on the state's population density measured in months. Based on the population density, it tells us how many months it will take to spread across the state.
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -69,6 +69,15 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+STATE_DATA.each do |state_name, numbers|
+  state = VirusPredictor.new(state_name, numbers[:population_density], numbers[:population])
+  state.virus_effects
+end
+
+#OR
+#STATE_DATA.each do |state_name, numbers|
+  #state = VirusPredictor.new(state_name, numbers[:population_density], numbers[:population]).virus_effects
+  #end
 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
@@ -85,3 +94,19 @@ alaska.virus_effects
 
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# One of keys uses strings with hash rockets and the other key uses symbols and colons.
+
+# What does require_relative do? How is it different from require?
+#The require_relative statement links the files together, it basically copies the input from another file and transfers it to the output file.
+#The difference between require_relative and require is the location of the files. If they're in the same directory, require_relative is used. If the other file is located in another directory, you would use require and list the file path.
+
+# What are some ways to iterate through a hash?
+# Creating a loop or using the .each method
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# The slight changes of the variables stood out the most.
+
+# What concept did you most solidify in this challenge?
+# I liked the extra practice of iterating through a hash.
