c227e6ef3516bff7fa54b682a09a14823b2b20cc
Lisa D'Asaro <lmdasaro@gmail.com>
2016-12-03 19:32:53 -0800

    complete 9.4 assignment.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,32 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+# write a GET route that displays
+# an address
+
+get '/contact' do
+  "1301 Third Ave #320, San Diego, CA 92101"
+end
+
+# write a GET route that takes
+# a persons name as a query parameter
+# and say "Good job, [person's name]!"
+get '/goodjob' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# A route that uses route parameters 
+# to add two numbers and respond 
+# with the result.
+get '/:n1/plus/:n2' do
+  n1 = params[:n1].to_i
+  n2 = params[:n2].to_i
+  "The sum is: #{n1 + n2}"
+end
